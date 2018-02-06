1c97c7005f4747ccffe68a464a85075e61d9c5e3
[GADD]Monkeynutz <harry@gamingatdeathsdoor.com>
2018-02-06 12:39:35 +0000

    Performance Updates, thanks [MGT]DB :)
    I had often thought about making this have simulation disabled for the objects but forgot about it. So, here's DB's edits and also my fixes to the wrong classnames being used in placed.

---
+++ @ExileServer/addons (NO LIGHTS)/satans_bridge/config.cpp
@@ -0,0 +1,19 @@
+class CfgPatches {
+	class satans_bridge {
+		units[] = {};
+		weapons[] = {};
+		requiredVersion = 0.1;
+		requiredAddons[] = {"exile_client","exile_server_config"};
+	};
+};
+class CfgFunctions {
+	class GADD {
+		class main {
+			file = "\x\addons\satans_bridge\init";
+			class init {
+				postInit = 1;
+			};
+		};
+	};
+};
+
---
+++ @ExileServer/addons (NO LIGHTS)/satans_bridge/init/fn_init.sqf
@@ -0,0 +1,6 @@
+// Include shit in here
+
+diag_log "Starting Satan's Bridge by [GADD]lords8n and [GADD]Monkeynutz";
+
+call compile preprocessFileLineNumbers "\x\addons\gadd_extras\mapcontent\satansbridge.sqf";
+call compile preprocessFileLineNumbers "\x\addons\gadd_extras\mapcontent\bridgemarkers.sqf";
