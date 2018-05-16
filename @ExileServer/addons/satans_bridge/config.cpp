aa517120a0dd837c6fea78b964c1e75da3f951c1
[GADD]Monkeynutz <harry@gamingatdeathsdoor.com>
2018-05-16 19:26:44 +0100

    Update config.cpp

--- @ExileServer/addons/satans_bridge/config.cpp
+++ @ExileServer/addons/satans_bridge/config.cpp
@@ -3,7 +3,7 @@ class CfgPatches {
 		units[] = {};
 		weapons[] = {};
 		requiredVersion = 0.1;
-		requiredAddons[] = {"exile_client","exile_server_config"};
+		requiredAddons[] = {};
 	};
 };
 class CfgFunctions {
